10bdcc5a79d587159d4e2ec9b70b8c2597b63cca
mudeitsi <eserenash@gmail.com>
2022-11-29 14:04:25 +0000

    ammended task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if(i is 8 and j is 9):
-            print("{}".format(str(i) + str(j)))
-        elif(j > i):
-            print("{}".format(str(i) + str(j)) + ", ", end="")
+for i in range(0, 8):
+    for j in range(i, 10):
+        if i != j:
+            print("{:d}{:d}, ".format(i, j), end='')
+print("89")
